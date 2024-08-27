495c8f428e2a857a288d6bfe290d789ac0cfe3c4
Luke Kacprowicz <65187097+Splaete@users.noreply.github.com>
2024-08-26 21:40:57 -0500

    Add files via upload

---
+++ data-types.py
@@ -0,0 +1,21 @@
+# This is a Python program to demonstrate different data types
+
+# Literal
+x = 5
+print("Literal:", x)
+
+# Integer
+y = -17
+print("Integer:", y)
+
+# Float
+z = 71.12
+print("Float:", z)
+
+# String
+name = "Natalia"
+print("String:", name)
+
+# Boolean
+is_active = False
+print("Boolean:", is_active)
