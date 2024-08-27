86ce2e47c2ea76f3565e25f612e434949a836e02
Luke Kacprowicz <65187097+Splaete@users.noreply.github.com>
2024-08-26 21:34:03 -0500

    Add files via upload

---
+++ joke.py
@@ -0,0 +1,10 @@
+# Luke Kacprowicz
+# Outputs the words in the quotes.
+print("Knock, knock.")  
+print("Who's there?")  
+print("Water")  
+print("Water who?")  
+print("Water you asking?")
+
+# Does the same as above but is in one line of code
+print("\nKnock, knock.\nWho's there?\nWater\nWater who?\nWater you asking?")
