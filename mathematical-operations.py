716196d74f8a23cb9ec76b9ea498ae2fdb3c875b
Luke Kacprowicz <65187097+Splaete@users.noreply.github.com>
2024-08-26 21:48:43 -0500

    Add files via upload

---
+++ mathematical-operations.py
@@ -0,0 +1,11 @@
+# The poitn of the code is to calculate and display the area of a rectangle
+
+# The variables for length and width
+length = 7
+width = 10
+
+# The calculation of the area of the rectangle
+area = length * width
+
+# Displaing the results while using escape sequences
+print("The area of the rectangle with a length of", length, "and a width of", width, "is", area,".")
